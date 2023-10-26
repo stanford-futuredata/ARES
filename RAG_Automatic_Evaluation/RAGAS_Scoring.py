68a4b26f03ada875067a3a1f04e35de7d572845c
jonsaadfalcon <jonsaadfalcon@gmail.com>
2023-10-25 18:28:44 -0700

    update

--- RAG_Automatic_Evaluation/RAGAS_Scoring.py
+++ RAG_Automatic_Evaluation/RAGAS_Scoring.py
@@ -3,6 +3,7 @@ from ragas import evaluate
 from datasets import Dataset
 import os
 import pandas as pd
+import scipy.stats as stats
 
 from ragas.metrics import (
     answer_relevancy,
@@ -14,10 +15,16 @@ from ragas.metrics import (
 #os.environ["OPENAI_API_KEY"] = "your-openai-key"
 
 evaluation_datasets = ['../datasets_v2/nq/ratio_0.7_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.725_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.75_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.775_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.8_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.825_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.85_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.875_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.9_reformatted_full_articles_False_validation_with_negatives.tsv']
-    
-for evaluation_dataset in evaluation_datasets[:1]:
+correct_ranking = [i for i in range(0, len(evaluation_datasets))]
+
+labels = ["Context Relevance", "Answer Faithfulness", "Answer Relevance"]
+context_scores = []
+answer_relevance_scores = []
+answer_faithfulness_scores = []
+
+for evaluation_dataset in evaluation_datasets[:2]:
     dataset = pd.read_csv(evaluation_dataset.replace("../", "../../ColBERT-FM/"), sep="\t")
-    dataset = dataset[:100]
+    dataset = dataset[:10]
 
     def string_to_list(text):
         return [text]
@@ -35,8 +42,31 @@ for evaluation_dataset in evaluation_datasets[:1]:
         faithfulness,
         answer_relevancy,
     ])
+
     print("Results for " + evaluation_dataset)
     print(results)
+
+    context_scores.append(results['context_precision'])
+    answer_relevance_scores.append(results['answer_relevancy'])
+    answer_faithfulness_scores.append(results['faithfulness'])
+
+####################################
+
+for label, scores in zip(labels, [context_scores, answer_relevance_scores, answer_faithfulness_scores]):
+
+    indexed_list = list(enumerate(scores))
+    sorted_list = sorted(indexed_list, key=lambda x: x[1])
+    sorted_indices = [index for index, _ in sorted_list]
+    tau, p_value = stats.kendalltau(correct_ranking, sorted_indices)
+
+    print("--------------------------------------------------")
+    print(label + " Scoring")
+    print("Correct Ranking v. LLM-as-a-Judge Ranking")
+    print(correct_ranking)
+    print(sorted_indices)
+    print("Kendall's Tau: " + str(tau))
+    print("P-Value: " + str(p_value))
+    print("--------------------------------------------------")
 
 
 # prepare your huggingface dataset in the format
