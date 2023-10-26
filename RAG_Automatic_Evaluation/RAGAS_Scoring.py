b6e3b53eda6111fb3fbb7ec620563df4fe3f22a2
jonsaadfalcon <jonsaadfalcon@gmail.com>
2023-10-25 18:32:42 -0700

    update

--- RAG_Automatic_Evaluation/RAGAS_Scoring.py
+++ RAG_Automatic_Evaluation/RAGAS_Scoring.py
@@ -15,6 +15,7 @@ from ragas.metrics import (
 #os.environ["OPENAI_API_KEY"] = "your-openai-key"
 
 evaluation_datasets = ['../datasets_v2/nq/ratio_0.7_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.725_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.75_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.775_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.8_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.825_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.85_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.875_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.9_reformatted_full_articles_False_validation_with_negatives.tsv']
+evaluation_datasets = evaluation_datasets[:2]
 correct_ranking = [i for i in range(0, len(evaluation_datasets))]
 
 labels = ["Context Relevance", "Answer Faithfulness", "Answer Relevance"]
@@ -22,7 +23,7 @@ context_scores = []
 answer_relevance_scores = []
 answer_faithfulness_scores = []
 
-for evaluation_dataset in evaluation_datasets[:2]:
+for evaluation_dataset in evaluation_datasets:
     dataset = pd.read_csv(evaluation_dataset.replace("../", "../../ColBERT-FM/"), sep="\t")
     dataset = dataset[:10]
 
