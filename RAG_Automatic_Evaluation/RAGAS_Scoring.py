c419a7654d645238b2447d49ee8d146def2b4a30
jonsaadfalcon <jonsaadfalcon@gmail.com>
2023-10-25 17:18:48 -0700

    update

--- RAG_Automatic_Evaluation/RAGAS_Scoring.py
+++ RAG_Automatic_Evaluation/RAGAS_Scoring.py
@@ -4,6 +4,13 @@ from datasets import Dataset
 import os
 import pandas as pd
 
+from ragas.metrics import (
+    answer_relevancy,
+    faithfulness,
+    context_recall,
+    context_precision,
+)
+
 #os.environ["OPENAI_API_KEY"] = "your-openai-key"
 
 evaluation_datasets = ['../datasets_v2/nq/ratio_0.7_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.725_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.75_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.775_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.8_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.825_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.85_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.875_reformatted_full_articles_False_validation_with_negatives.tsv', '../datasets_v2/nq/ratio_0.9_reformatted_full_articles_False_validation_with_negatives.tsv']
@@ -17,7 +24,11 @@ for evaluation_dataset in evaluation_datasets[:1]:
     dataset = dataset.rename_column("Answer", "answer")
     dataset = dataset.rename_column("Document", "contexts")
 
-    results = evaluate(dataset)
+    results = evaluate(dataset, metrics=[
+        context_precision,
+        faithfulness,
+        answer_relevancy,
+    ])
     print("Results for " + evaluation_dataset)
     print(results)
 
