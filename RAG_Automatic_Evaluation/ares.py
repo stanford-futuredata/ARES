dffeee09999a30b425ec989e5894284ae8e27a41
Robby Manihani <manihani@stanford.edu>
2024-01-17 19:05:43 -0800

    Added score averages.

--- RAG_Automatic_Evaluation/ares.py
+++ RAG_Automatic_Evaluation/ares.py
@@ -2,6 +2,7 @@ import subprocess
 from Evaluation_Functions import few_shot_answer_faithfulness_scoring 
 from Evaluation_Functions import few_shot_answer_relevance_scoring
 from Evaluation_Functions import few_shot_context_relevance_scoring
+from Evaluation_Functions import calculate_accuracy
 import pandas as pd
 
 class ARES: 
@@ -38,6 +39,9 @@ class ARES:
 ##############################################################################################################################
     
     def run(self): 
+        context_relevance_answers = self.unlabeled_evaluation_set["Context_Relevance_Label"].tolist()
+        answer_relevance_answers = self.unlabeled_evaluation_set["Answer_Relevance_Label"].tolist()
+        answer_faithfulness_answers = self.unlabeled_evaluation_set["Answer_Faithfulness_Label"].tolist()
         context_relevance_scores = []
         answer_relevance_scores = []
         answer_faithfulness_scores = []
@@ -63,8 +67,55 @@ class ARES:
 
         # Compile results into a dictionary
         return {
-            "Context Relevance Scores": context_relevance_scores,
-            "Answer Faithfulness Scores": answer_faithfulness_scores,
-            "Answer Relevance Scores": answer_relevance_scores
+            "Context Relevance Scores": round(sum(context_relevance_scores)/len(context_relevance_scores), 3),
+            "Answer Faithfulness Scores": round(sum(answer_faithfulness_scores)/len(answer_faithfulness_answers), 3),
+            "Answer Relevance Scores": round(sum(answer_relevance_scores)/len(answer_relevance_answers), 3)
         }
 
+
+#     def run(self): 
+
+#         context_relevance_score = few_shot_context_relevance_scoring(self.context_relevance_system_prompt,self.unlabeled_evaluation_set,self.unlabeled_evaluation_set, "gpt-3.5-turbo", self.in_domain_prompts_dataset)
+#         answer_relevance_score = few_shot_answer_relevance_scoring(self.answer_relevance_system_prompt,self.unlabeled_evaluation_set,self.unlabeled_evaluation_set, "gpt-3.5-turbo",self.in_domain_prompts_dataset)
+#         answer_faithfulness_score = few_shot_answer_faithfulness_scoring(self.answer_faithfulness_system_prompt,self.unlabeled_evaluation_set,self.unlabeled_evaluation_set, "gpt-3.5-turbop",self.in_domain_prompts_dataset)
+    
+#         return {
+#                 "Context Relevance Scores": context_relevance_scores,
+#                 "Answer Faithfulness Scores": answer_faithfulness_scores,
+#                 "Answer Relevance Scores": answer_relevance_scores
+#             }
+
+# ares = ARES("/future/u/manihani/ARES/example_files/evaluation_datasets.tsv", "/future/u/manihani/ARES/example_files/few_shot_prompt_filename.tsv", gpt_model = "text-davinci-003")
+# results = ares.run()
+# print(results)
+
+
+    # def run(self):
+    #     # Generate Synthetic Data
+    #     synthetic_queries_filename = "output/synthetic_queries.tsv"
+    #     documents_sampled = "10000"
+    #     self._generate_synthetic_data(synthetic_queries_filename, documents_sampled)
+
+    #     # Fine-tune LLM-as-a-Judge
+    #     model_checkpoint_path = self._fine_tune_llm(synthetic_queries_filename)
+
+    #     # Score RAG System
+    #     scores = self._score_rag_system(model_checkpoint_path)
+    #     return scores
+
+    # def generate_synthetic_data(self):
+    #     # Set default values for parameters not provided
+    #     synthetic_queries_filename = "output/synthetic_queries.tsv"
+
+    #     # Synthetic data generation
+    #     subprocess.run([
+    #         "python", "LLM-as-a-Judge_Adaptation/Generate_Synthetic_Queries_and_Answers.py",
+    #         "--document_filepath", self.in_domain_prompts_dataset,
+    #         "--few_shot_prompt_filename", self.in_domain_prompts_dataset,
+    #         "--synthetic_queries_filename", synthetic_queries_filename,
+    #         "--documents_sampled", documents_sampled
+    #     ])
+    
+    # def fine_tune_llm(self): 
+    #     num_epochs = 10 
+    #     patience_value = 
